fffd941eae120344ee7655973078edb5720fe769
Vitaly <vitalyb@gmail.com>
2021-07-22 15:25:26 +0300

    reports - added project

---
+++ apps/reports/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: "reports",
+  roots: ["src"],
+  preset: "../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/apps/reports",
+};
---
+++ apps/reports/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <StrictMode>
+    <div>Hello</div>
+  </StrictMode>,
+  document.getElementById("root")
+);
