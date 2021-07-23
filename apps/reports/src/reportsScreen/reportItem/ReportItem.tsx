a553b5b273caff2d9ad94791f06da76eb12b4650
Vitaly <vitalyb@gmail.com>
2021-07-24 00:00:58 +0300

    reports - transforms

--- apps/reports/src/reportsScreen/reportItem/ReportItem.tsx
+++ apps/reports/src/reportsScreen/reportItem/ReportItem.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
-import { loggerCreator } from "common/utils/logger";
-import { CommonUrls } from "common/utils/commonUrls";
-import { UrlParameterTypeEnum } from "common/stores/urlParameterTypeEnum";
-import { QnDeploymentEntity } from "common/domain/qwiltDeployment/qnDeploymentEntity";
+import { loggerCreator } from "@qwilt/common/utils/logger";
+import { CommonUrls } from "@qwilt/common/utils/commonUrls";
+import { UrlParameterTypeEnum } from "@qwilt/common/stores/urlParameterTypeEnum";
+import { QnDeploymentEntity } from "@qwilt/common/domain/qwiltDeployment/qnDeploymentEntity";
 
-const moduleLogger = loggerCreator(__filename);
+const moduleLogger = loggerCreator("__filename");
 
 //region [[ Styles ]]
 
