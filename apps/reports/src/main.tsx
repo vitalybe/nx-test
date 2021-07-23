abb363ba4eb691f9aa107ada5998a648c5720176
Vitaly <vitalyb@gmail.com>
2021-07-24 00:04:24 +0300

    tsc passes

--- apps/reports/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import * as ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <StrictMode>
-    <div>Hello</div>
-  </StrictMode>,
-  document.getElementById("root")
-);
