a9c52347acb0fff51829dc5e571423828d505f12
Vitaly <vitalyb@gmail.com>
2021-07-24 22:56:55 +0300

    all images are showing

--- config/webpack/webpackCustomized.config.js
+++ config/webpack/webpackCustomized.config.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const nrwlConfig = require("@nrwl/react/plugins/webpack.js");
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = (config, context) => {
   nrwlConfig(config);
@@ -19,3 +19,40 @@ module.exports = (config, context) => {
     plugins: [...config.plugins],
   };
 };
+
+// Copied from: /Users/vitalyb/git-temp/nx/packages/react/plugins/webpack.ts
+function nrwlConfig(config) {
+  // TODO(jack): Remove in Nx 13
+  // const { isWebpack5 } = require('@nrwl/web/src/webpack/entry');
+  const isWebpack5 = false;
+  config.module.rules.push({
+    test: /\.(png|jpe?g|gif|webp|svg)$/,
+    loader: require.resolve("url-loader"),
+    options: {
+      limit: 10000, // 10kB
+      name: "[name].[hash:7].[ext]",
+      esModule: false,
+    },
+  });
+
+  // TODO(jack): support webpack 5
+  if (!isWebpack5 && config.mode === "development" && config["devServer"]?.hot) {
+    // add `react-refresh/babel` to babel loader plugin
+    const babelLoader = config.module.rules.find((rule) => rule.loader.toString().includes("babel-loader"));
+    if (babelLoader) {
+      babelLoader.options["plugins"] = [
+        ...(babelLoader.options["plugins"] || []),
+        [
+          require.resolve("react-refresh/babel"),
+          {
+            skipEnvCheck: true,
+          },
+        ],
+      ];
+    }
+    // add https://github.com/pmmmwh/react-refresh-webpack-plugin to webpack plugin
+    config.plugins.push(new ReactRefreshPlugin());
+  }
+
+  return config;
+}
