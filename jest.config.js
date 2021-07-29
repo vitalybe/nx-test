00014971e25cfa08cdcdd107ca88edbecb8c9ac2
Vitaly <vitalyb@gmail.com>
2021-07-29 11:12:18 +0300

    delivery-agreements-management - added project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ["<rootDir>/libs/common", "<rootDir>/apps/reports"],
+  projects: ["<rootDir>/libs/common", "<rootDir>/apps/reports", "<rootDir>/apps/delivery-agreements-management"],
 };
