f684623dc0028122ef5ae8f0608bc89b531dd61a
Vitaly <vitalyb@gmail.com>
2021-08-06 17:51:08 +0300

    executors - auto compile to js on launch

--- tools/executors/echo/impl.js
+++ tools/executors/echo/impl.js
@@ -1,17 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const child_process_1 = require("child_process");
-const util_1 = require("util");
-function echoExecutor(options, context) {
-    return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        console.info(`Executing "echo"...`);
-        console.info(`Options: ${JSON.stringify(options, null, 2)}`);
-        const { stdout, stderr } = yield util_1.promisify(child_process_1.exec)(`echo ${options.textToEcho}`);
-        console.log(stdout);
-        console.error(stderr);
-        return { success: true };
-    });
-}
-exports.default = echoExecutor;
-//# sourceMappingURL=impl.js.map
+require('./ts-hook');
+module.exports = require('./impl.ts');
